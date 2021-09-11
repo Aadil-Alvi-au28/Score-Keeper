3ac4dd0bdf8a7031d73b6c80f0753efd4f1cf991
Mohammed Aadil Alvi <75182081+Mohammed-Aadil-Alvi@users.noreply.github.com>
2021-09-11 11:44:21 +0530

    Add files via upload

---
+++ score_keeper.js
@@ -0,0 +1,60 @@
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display'),
+}
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display'),
+}
+
+
+let resetButton = document.querySelector('#reset')
+const selectWinningScore = document.querySelector('#playTo')
+
+let winningScore = 3;
+let isGameOver = false;
+
+function updateScore(player, opponent) {
+    if (!isGameOver) {
+        player.score += 1;
+        if (player.score === winningScore) {
+            isGameOver = true;
+            player.display.classList.add('has-text-success')
+            opponent.display.classList.add('has-text-danger')
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+
+
+        }
+        player.display.textContent = player.score;
+    }
+}
+
+p1.button.addEventListener('click', function () {
+    updateScore(p1, p2);
+})
+p2.button.addEventListener('click', function () {
+    updateScore(p2, p1);
+
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset() {
+    isGameOver = false;
+    p1.score = 0;
+    p2.score = 0;
+    p1.display.textContent = 0;
+    p2.display.textContent = 0;
+    p1.display.classList.remove('has-text-success', 'has-text-danger')
+    p2.display.classList.remove('has-text-success', 'has-text-danger')
+    p1.button.disabled = false;
+    p2.button.disabled = false;
+}
+
+selectWinningScore.addEventListener('change', function () {
+    winningScore = parseInt(this.value)
+    reset();
+})
